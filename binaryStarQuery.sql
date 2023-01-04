ef01e8a855086e578d533e7cfef173d56a4a1eb0
huytrinh03 <trinhnhathuy03@gmail.com>
2023-01-04 17:30:02 -0500

    delete

--- binaryStarQuery.sql
+++
@@ -1,6 +0,0 @@
--- This query finds binary stars from apogeeStar
-
-SELECT star.apogee_id, star.apstar_id, star.ra, star.dec, star.nvisits,
-star.snr, star.vscatter, star.verr
-FROM apogeeStar as star
-WHERE star.nvisits >= 8 AND star.vscatter > 10*star.verr AND star.snr >= 5
