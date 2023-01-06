be6327e07dcc0f72650fd481fd537f55ad72eb4a
huytrinh03 <97802661+huytrinh03@users.noreply.github.com>
2023-01-05 23:55:20 -0500

    Add files via upload

--- binaryStarVisit1000Query.sql
+++ binaryStarVisit1000Query.sql
@@ -1,5 +1,5 @@
--- leave out those who have bitmask #4, #19, #22,
--- caution with those who have bitmask #3, #18, #21, #0
+-- leave out those who have bitmask #0, #4, #19, #22,
+-- caution with those who have bitmask #3, #18, #21
 
 SELECT TOP 1000
 star.apogee_id, star.apstar_id, star.ra, star.dec, star.nvisits,
@@ -9,7 +9,7 @@ visit.vhelio, visit.vrelerr, visit.snr as visitSNR, visit.starflag, visit.mjd
 FROM apogeeStar as star
 JOIN apogeeStarVisit as starVisit on star.apstar_id = starVisit.apstar_id
 JOIN apogeeVisit as visit on starVisit.visit_id = visit.visit_id
-WHERE star.nvisits >= 8 AND star.vscatter > 10*star.verr AND star.snr >= 5
+WHERE star.nvisits >= 8 AND star.vscatter > 1 AND star.vscatter > 10*star.verr AND star.snr >= 5
 AND visit.starflag % POWER(2,1) < POWER(2,0) --Condition for 0th to be 0
 AND visit.starflag % POWER(2,5) < POWER(2,4) --Condition for 4th to be 0
 AND visit.starflag % POWER(2,20) < POWER(2,19) --Condition for 19th bit to be 0
